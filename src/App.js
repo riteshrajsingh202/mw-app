9c8b53a03459b96040c52657f692caea93a7bb83
riteshrajsingh202 <riteshrajsingh202@gmail.com>
2022-11-13 23:45:57 +0530

    first push

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Embedding from './component/Embedding';
+function App() {
+  return (
+    <div className="App">
+      <h1>Basic Embbed</h1>
+    <Embedding/>
+    </div>
+  );
+}
+
+export default App;
